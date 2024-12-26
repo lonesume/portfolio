617ec4d96b5d949e0520de009f5dce3f49cfc98c
Brian Joseph <brianjoseph1758@gmail.com>
2024-12-25 23:34:09 -0500

    Add logos to carousel

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,12 +5,17 @@ import "swiper/css";
 import "swiper/css/navigation"; // Import Swiper's navigation CSS
 import { Navigation } from "swiper/modules"; // Correct import path for Navigation module
 
+import pocketGpt from "~/assets/images/projects/pocket-gpt.png";
+import weatherApp from "~/assets/images/projects/weather_app_logo.png";
+import bridge from "~/assets/images/projects/bridge-chatgpt-logo.png";
+import motion from "~/assets/images/projects/motion-capture-emailer-logo.png";
+import Image from "next/image";
+
 export default function Projects() {
   return (
     <>
       <Navbar />
       <BodyContainer>
-        <p>Projects</p>
         <Swiper
           spaceBetween={50}
           slidesPerView={3}
@@ -21,29 +26,32 @@ export default function Projects() {
           onSwiper={(swiper) => console.log(swiper)}
         >
           <SwiperSlide>
-            <img
-              src="https://via.placeholder.com/300x200"
+            <Image
+              // src="https://via.placeholder.com/300x200"
+              src={pocketGpt}
               alt="Slide 1"
               style={{ width: "100%", height: "auto" }}
             />
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src="https://via.placeholder.com/300x200"
+            <Image
+              // src="https://via.placeholder.com/300x200"
+              src={weatherApp}
               alt="Slide 2"
               style={{ width: "100%", height: "auto" }}
             />
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src="https://via.placeholder.com/300x200"
+            <Image
+              // src="https://via.placeholder.com/300x200"
+              src={bridge}
               alt="Slide 3"
               style={{ width: "100%", height: "auto" }}
             />
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src="https://via.placeholder.com/300x200"
+            <Image
+              src={motion}
               alt="Slide 4"
               style={{ width: "100%", height: "auto" }}
             />
