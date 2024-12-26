846e83428443673affb2f9ca2aa1eb7fa97b050f
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-25 22:03:21 -0500

    Add QOL changes to about and to navbar

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,29 @@
 import BodyContainer from "~/components/body-container";
 import Navbar from "~/components/navbar";
+import { useScramble } from "use-scramble";
 
 export default function About() {
+  const { ref } = useScramble({
+    text: "brian@brianjoseph.me",
+    range: [65, 125],
+    speed: 0.5,
+    tick: 2,
+    step: 5,
+    scramble: 5,
+    seed: 2,
+    chance: 1,
+    overdrive: false,
+    overflow: false,
+  });
+
   return (
     <>
       <Navbar />
       <BodyContainer>
-        <p className="py-24 text-center text-3xl text-yellow-300">
-          brian@brianjoseph.me
-        </p>
+        <p className="py-24 text-center text-3xl text-yellow-300" ref={ref}></p>
         <div className="columns-3 px-52">
           <p className="break-inside-avoid">
-            Hi, I'm Brian! I am a student at Valencia College
+            Hi, I&apos;m Brian! I am a student at Valencia College
           </p>
           <p className="break-inside-avoid">
             I currently study Computer Science, with an emphasis on web
@@ -22,14 +34,15 @@ export default function About() {
           <p className="break-inside-avoid">
             Currently, I work at The Shack, flexing my engineering prowess by
             meticulously crafting the finest delicacies for my loyal
-            customers—because nothing says "future software engineer" like
-            optimizing the perfect cheese-to-burger ratio and debugging
+            customers—because nothing says &quot;future software engineer&quot;
+            like optimizing the perfect cheese-to-burger ratio and debugging
             milkshake machines in record time.{" "}
           </p>
           <p className="break-inside-avoid">
             In the future, I would like to work in big tech, specifically
             because I love how these companies are able to create innovative
-            solutions that simplify and improve people's lives in so many ways.
+            solutions that simplify and improve people&apos;s lives in so many
+            ways.
           </p>
           <p className="break-inside-avoid">
             Also, outside of work and studying, I like to go to the gym, where I
