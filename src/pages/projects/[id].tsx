8c71672bcfcc3da325a5df382e9ad8f3a5678875
Brian Joseph <brianjoseph1758@gmail.com>
2024-12-26 16:56:13 -0500

    Render image on project details page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
 import BodyContainer from "~/components/body-container";
 import Navbar from "~/components/navbar";
-// import { useEffect, useState } from "react";
+import { details } from "~/utils/projects";
+import Image from "next/image";
+import { useMemo } from "react";
 
 const ProjectPage = () => {
   const router = useRouter();
@@ -12,14 +14,33 @@ const ProjectPage = () => {
     return <p>Loading...</p>; // Return a loading state or skeleton screen
   }
 
-  // Render the actual content
+  const project = useMemo(
+    () => details.filter((detail) => detail.id === id)[0]!,
+    [details],
+  );
+
+  const renderProject = (): JSX.Element => {
+    return (
+      <Image
+        src={project.imageUrl}
+        alt={project.projectName}
+        objectFit="cover"
+        width={1024} // Use numbers for width and height
+        height={1024}
+      />
+    );
+  };
+
   return (
     <>
       <Navbar />
       <BodyContainer>
-        <div>
-          <h1>Project: {id}</h1>
-          <p>This is the project details page for {id}.</p>
+        <div className="flex flex-row">
+          <div className="w-2/3">
+            <h1 className="text-4xl">Project: {project.projectName}</h1>
+            <p>This is the project details page for {project.projectName}.</p>
+          </div>
+          <div className="w-1/3">{renderProject()}</div>
         </div>
       </BodyContainer>
     </>
